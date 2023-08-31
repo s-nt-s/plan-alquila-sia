bd1b129f891a83cbb769c6b1eb77cbdd75e7ca6a
s-nt-s <santos82@gmail.com>
2023-08-31 14:04:27 +0200

    reducir tabla

--- core/piso.py
+++ core/piso.py
@@ -58,3 +58,45 @@ class Piso:
 
     def items(self):
         return self.asdict().items()
+
+    def get_plan_id(self):
+        ord = tuple(sorted('alq sia'.split()))
+        pla = self.plan.lower()
+        if pla not in ord:
+            return self.id
+        return ((ord.index(pla)+1)*10000000)+self.id
+
+    def get_planta_title(self):
+        planta = self.planta
+        if planta == 0:
+            planta = "Bajo"
+        if isinstance(planta, int):
+            planta = str(planta)+'º'
+        if self.ascensor:
+            planta = planta+" con "
+        else:
+            planta = planta+" sin "
+        planta = planta + "ascensor"
+        return planta
+    
+    def get_direccion(self):
+        spl = self.direccion.split(None, 1)
+        if len(spl) != 2:
+            return self.direccion
+        dire = spl[1]
+        tipo = spl[0].lower()
+        if tipo == "calle":
+            return "C/ "+dire
+        if tipo in ("avenida", 'avda'):
+            return "Av. "+dire
+        if tipo == "paseo":
+            return "Pº "+dire
+        if tipo in ('travesía', 'trva'):
+            return "Trª "+dire
+        if tipo == "plaza":
+            return "Pl. "+dire
+        if tipo == 'ronda':
+            return "Rda. "+dire
+        #if tipo in ('camino', 'cmno'):
+        #    return "Cam. "+dire
+        return self.direccion
