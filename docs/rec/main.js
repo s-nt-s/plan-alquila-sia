083de05a6ae38de9b58cee46032271e533aae304
s-nt-s <santos82h@gmail.com>
2023-09-10 21:38:27 +0200

    filtar por zona

--- docs/rec/main.js
+++ docs/rec/main.js
@@ -1,23 +1,13 @@
 function filtrar() {
+    const zona = document.getElementById("zona").value.trim();
     const trs = document.querySelectorAll("tr[data-zona]");
-    const show = Array.from(document.querySelectorAll("#zonas input")).filter(i=>i.checked).map(i=>i.value);
-    if (show.length==0) {
-        trs.forEach(tr=>{
-            tr.style.display = '';
-        })
-        return;
-    }
     trs.forEach(tr=>{
-        if (show.includes(tr.getAttribute("data-zona"))) tr.style.display = '';
+        if (zona.length==0 || zona==tr.getAttribute("data-zona")) tr.style.display = '';
         else tr.style.display = 'none';
     })
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    const npt = document.querySelectorAll("#zonas input");
-    if (npt.length==0) return;
-    document.querySelectorAll("#zonas input").forEach(i=> {
-        i.addEventListener("click", filtrar)
-    })
+    document.getElementById("zona").addEventListener("change", filtrar)
     filtrar();
 });
